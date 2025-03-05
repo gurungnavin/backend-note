05cc55672ee61a390067e33f14c78ceae92beb25
gurungnavin <gurungnavin@icloud.com>
2025-03-05 17:35:55 +0900

    logout controller with custom middlerware, add route of login and logout with verifyJwt middleware

---
+++ codes/04_backend-professional-project/src/middlewares/auth.middlerware.js
@@ -0,0 +1,40 @@
+import jwt from "jsonwebtoken"
+import {User} from "../models/user.models.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+
+export const verifyJwt = asyncHandler(async(req, _, next)=> {
+  // NOTE: if res is not used, we can write underScore '_'
+  try {
+    //access token from cookie of req., because loggedIn user has token
+    const token = req.cookie?.accessToken || req.header("Authorization"?.replace("Bearer ", ""))
+
+
+    //check that token if not throw error
+    if(!token){
+      throw new ApiError(409, "Unauthorized request" )
+    }
+
+    // if token, now verify that token with access_secret_token
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+    //next ->search by _id and delete password and refresh token 
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    )
+
+    if(!user) {
+      throw new ApiError(410, "Invalid Access Token")
+    }
+
+    // if all okay, save user on req.user
+
+    req.user = user
+
+    
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access Token")
+  }
+  next()
+})
--- codes/04_backend-professional-project/src/routes/user.routes.js
+++ codes/04_backend-professional-project/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express"
-import { userRegister } from "../controllers/user.controller.js"
+import { userRegister, userLogin, userLogOut } from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middlerware.js"
+import { verifyJwt } from "../middlewares/auth.middlerware.js"
 
 const router = Router()
 
@@ -14,5 +15,8 @@ router.route("/register").post(upload.fields([
     maxCount: 1,
   }
 ]), userRegister)
+
+router.route("/login").post(userLogin)
+router.route("/logout").post(verifyJwt, userLogOut)
 
 export default router;
